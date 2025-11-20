54a479bb39371938bb3705deadeb3ffbb32993d5
李春春 <lilly@lichunchundeMacBook-Air.local>
2025-11-20 17:45:23 +0800

    fix client floder

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import './Chat.css';
+
+let socket;
+
+
+
+const Chat = ({ location }) => {
+    const search = (location && location.search) || window.location.search;
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = 'http://localhost:5001';
+    
+    
+    
+    useEffect(() => {
+      const { name, room } = queryString.parse(search);
+
+      socket = io(ENDPOINT);
+
+      setRoom(room);
+      setName(name);
+
+      socket.emit('join', { name, room }, () => {
+
+      });
+
+      return () => {
+        socket.emit('disconnect');
+
+        socket.off();
+      }
+      
+    }, [ENDPOINT, search]);
+
+    useEffect(() => {
+      socket.on('message', (message) => {
+        setMessages([ ...messages, message ]);
+      })
+    }, [messages]);
+
+    const sendMessage = (event) => {
+      event.preventDefault();
+
+      if(message) {
+        socket.emit('sendMessage', message, () => setMessage(''));
+      }
+    }
+    
+    console.log(message, messages);
+
+    return (
+        <div className='outerContainer'>
+          <div className='container'>
+            <InfoBar room={room}/>
+            <Messages messages={messages} name={name}/>
+            <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+          </div>
+        </div>
+    )
+}
+
+export default Chat;
